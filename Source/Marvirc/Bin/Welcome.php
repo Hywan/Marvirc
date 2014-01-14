90c8bebe6e071fd39557cb00a2fc28528e53e815
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-01-14 20:54:53 +0100

    Write the `welcome` command.

--- Source/Marvirc/Bin/Welcome.php
+++ Source/Marvirc/Bin/Welcome.php
@@ -4,6 +4,7 @@ namespace Marvirc\Bin {
 
 use Hoa\Console\Dispatcher\Kit;
 use Hoa\Console\GetOption;
+use Hoa\Console\Cursor;
 
 class Welcome extends Kit {
 
@@ -27,6 +28,38 @@ class Welcome extends Kit {
                 $this->resolveOptionAmbiguity($v);
               break;
         }
+
+        echo 'Hello, I am Marvirc, a dead ';
+        Cursor::colorize('foreground(yellow)');
+        echo 'simple';
+        Cursor::colorize('normal');
+        echo ', extremely ';
+        Cursor::colorize('foreground(yellow)');
+        echo 'modular';
+        Cursor::colorize('normal');
+        echo ' and blazzing ';
+        Cursor::colorize('foreground(yellow)');
+        echo 'fast IRC bot';
+        Cursor::colorize('normal');
+        echo ' (yup, that\'s it).', "\n\n",
+
+             'Here is the list of my available commands:', "\n",
+             '  * ';
+        Cursor::colorize('foreground(green)');
+        echo 'join';
+        Cursor::colorize('normal');
+        echo ': connect Marvirc to a channel.', "\n\n",
+
+             'That\'s all for now.', "\n",
+             'Type:', "\n", '    ';
+        Cursor::colorize('foreground(yellow)');
+        echo $_SERVER['argv'][0], ' ';
+        Cursor::colorize('foreground(green)');
+        echo 'command';
+        Cursor::colorize('foreground(yellow)');
+        echo ' --help';
+        Cursor::colorize('normal');
+        echo ' to get more informations.', "\n";
     }
 
     public function usage ( ) {
